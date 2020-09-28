85e6b76dc9588ecfa276fc0630dc771a50b915f8
Johann RAMANANDRAITSIORY <matrixzeyphax@gmail.com>
2020-09-28 16:20:50 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("Fraction Launcher");
+		Fraction f = new Fraction(10,5);
+		System.out.println(f.toString());
 	}
 }
