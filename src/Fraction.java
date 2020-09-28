322d0c63c2e361644cca6ab690534dc7763b5607
Johann RAMANANDRAITSIORY <matrixzeyphax@gmail.com>
2020-09-28 16:19:39 +0200

    Fraction.java

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,3 +1,24 @@
 public class Fraction
 {
+    // Variable d'instances //
+    private int numerateur;
+    private int denominateur;
+
+    public Fraction()
+    {
+        this.numerateur = 1;
+        this.denominateur = 1;
+    }
+
+    public Fraction(int a, int b)
+    {
+        this.numerateur = a;
+        this.denominateur = b;
+    }
+
+    @Override
+    public String toString()
+    {
+        return numerateur + " / " + denominateur;
+    }
 }
